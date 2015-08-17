17f97694f4d22bf66d3e5cd7d4c0992ec3c2e21c
Rowan Winsemius <rwinsemius@geoplex.com.au>
2015-08-17 23:40:40 +1000

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # turf-builder
 A simple user interface for generating custom browserify builds of Turf.js
+
+This simple UI enables users to generate custom browserify builds of Turf.js without the commandline.
+
+###Installation
+Download the code and place it in a folder called 'Build' in folder containing a copy of the Turf repo. Once you've done that run the following in your command line from the build directory 
+````
+npm install
+````
+
+###Starting the application
+In the command line type 
+````
+node index.js
+````
+This should automatically open up a new browser window with the application, if not try navigating to (http://localhost:5000)[http://localhost:5000]
+
+###Use
+Simply check the boxes on the interface and hit 'Build', this will generate a new file and place it in the 'Output' directory.
---
+++ index.js
@@ -0,0 +1,76 @@
+var fs = require("fs");
+var path = require("path");
+var express = require("express");
+var app = express();
+var browserify = require('browserify');
+var opener = require("opener");
+
+app.set('view engine', 'ejs');
+
+app.get("/", function(req, res){
+	res.render("index", {
+		turfModules: turfModules
+	});
+});
+
+app.get("/build", function(req, res){
+
+	var requiredModules = req._parsedUrl.query.split(",");
+	//Create the module exports requirements 	
+	var outputFileString = "module.exports = {";
+	for (var i = 0; i < requiredModules.length; i++ ) {
+		var plainModuleName = requiredModules[i].replace("turf-","");
+		outputFileString += plainModuleName + ": require('"+ requiredModules[i] +"'),";
+	}
+	outputFileString = outputFileString.substring(0, outputFileString.length - 1);
+	outputFileString += "}";
+
+	//Create a temp file listing the module exports
+	fs.writeFile(__dirname + '/outputs/temp.js', outputFileString);
+
+	var b = browserify('./outputs/temp.js',{
+		standalone:"turf"
+	});
+
+	b.transform({
+		global: true
+	}, 'uglifyify');
+	
+	var d = new Date();
+	d = d.toDateString();
+	d = d.replace(/ /g, '');
+	var filename = "turf_" + d+".min.js";
+	var writeFile = fs.createWriteStream(__dirname + '/outputs/'+filename);
+
+	b.bundle().pipe(writeFile);
+
+	writeFile.on('finish', function(){
+		fs.unlink(__dirname + '/outputs/temp.js');
+	});
+
+	res.render("complete");
+});
+
+var port = 5000;
+app.listen(port, function() {
+	opener("http://localhost:5000");
+	console.log("The Turf build tool has been started at http://localhost:5000, all going well it should open automatically!");
+});
+
+
+function getDirectories(srcpath) {
+	return fs.readdirSync(srcpath).filter(function(file) {
+		return fs.statSync(path.join(srcpath, file)).isDirectory();
+	});
+}
+
+var allModules = getDirectories('../node_modules');
+var turfModules =[];
+
+for (i = 0; i < allModules.length; i++) { 
+	if (allModules[i].indexOf("turf") > -1){
+		turfModules.push(allModules[i]); 
+	}
+}
+
+
