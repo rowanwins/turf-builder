ae8c30faa21a053f39d132521292b2653e51b71e
Rowan Winsemius <rwinsemius@geoplex.com.au>
2015-08-20 11:01:35 +1000

    Improved error handling for startup. Support for custom file names.

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 var fs = require("fs");
 var path = require("path");
 var express = require("express");
-var app = express();
-var browserify = require('browserify');
 var opener = require("opener");
+
+var browserify;
+
+var app = express();
 
 app.set('view engine', 'ejs');
 
@@ -15,17 +17,17 @@ app.get("/", function(req, res){
 
 app.get("/build", function(req, res){
 
-	var requiredModules = req._parsedUrl.query.split(",");
-	//Create the module exports requirements 	
+	var requiredModules = req.query.modules.split(",");
 	var outputFileString = "module.exports = {";
+
 	for (var i = 0; i < requiredModules.length; i++ ) {
 		var plainModuleName = requiredModules[i].replace("turf-","");
 		outputFileString += plainModuleName + ": require('"+ requiredModules[i] +"'),";
 	}
+
 	outputFileString = outputFileString.substring(0, outputFileString.length - 1);
 	outputFileString += "}";
 
-	//Create a temp file listing the module exports
 	fs.writeFile(__dirname + '/outputs/temp.js', outputFileString);
 
 	var b = browserify('./outputs/temp.js',{
@@ -36,10 +38,16 @@ app.get("/build", function(req, res){
 		global: true
 	}, 'uglifyify');
 	
-	var d = new Date();
-	d = d.toDateString();
-	d = d.replace(/ /g, '');
-	var filename = "turf_" + d+".min.js";
+	var filename;
+	if (!req.query.fn){
+		var d = new Date();
+		d = d.toDateString();
+		d = d.replace(/ /g, '');
+		filename = "turf_" + d+".min.js"; 
+	}
+	else {
+		filename = req.query.fn.toString()+".min.js";
+	}
 	var writeFile = fs.createWriteStream(__dirname + '/outputs/'+filename);
 
 	b.bundle().pipe(writeFile);
@@ -51,12 +59,26 @@ app.get("/build", function(req, res){
 	res.render("complete");
 });
 
-var port = 5000;
-app.listen(port, function() {
-	opener("http://localhost:5000");
-	console.log("The Turf build tool has been started at http://localhost:5000, all going well it should open automatically!");
-});
 
+var turfModules =[];
+var turfLocation = '../node_modules';
+
+var startup = (function checkExistance(){
+	
+	fs.lstat(turfLocation, function (err, inodeStatus) {
+		if (err){
+			console.log("Hmmm there was an error and we couldn't find any turf modules... please double check the install instuctions");
+			return;
+		}
+
+		var allModules = getDirectories(turfLocation);
+		createModuleArray(allModules);
+		checkCreateOutputDirectory();
+		startServer();
+		
+		return "modules could be found";
+	});
+})();
 
 function getDirectories(srcpath) {
 	return fs.readdirSync(srcpath).filter(function(file) {
@@ -64,13 +86,33 @@ function getDirectories(srcpath) {
 	});
 }
 
-var allModules = getDirectories('../node_modules');
-var turfModules =[];
-
-for (i = 0; i < allModules.length; i++) { 
-	if (allModules[i].indexOf("turf") > -1){
-		turfModules.push(allModules[i]); 
+function createModuleArray(allModules){
+	for (i = 0; i < allModules.length; i++) { 
+		if (allModules[i].indexOf("turf") > -1){
+			turfModules.push(allModules[i]); 
+		}
 	}
 }
 
+function startServer(){
+	browserify = require('browserify');
+	var port = 5000;
+	app.listen(port, function() {
+		opener("http://localhost:5000");
+		console.log("The Turf build tool has been started at http://localhost:5000, all going well it should open automatically!");
+	});
+}
 
+
+
+function checkCreateOutputDirectory(){
+	
+	fs.lstat('./outputs', function (err, inodeStatus) {
+		if (err){
+			fs.mkdir('./outputs');
+			return;
+		}
+		
+		return "output directory already exists";
+	});
+}
